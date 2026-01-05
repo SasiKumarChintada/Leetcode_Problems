c452e70312f75843c491e36ead36d14a53ee893c
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-01-05 23:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (15.59%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int>prefix(gain.size()+1);
+        prefix[0]=0;
+        for(int i=1;i<=gain.size();i++){
+            prefix[i]=prefix[i-1]+gain[i-1];
+        }
+        int ans=*max_element(prefix.begin(),prefix.end());
+        return ans;
+    }
+};
