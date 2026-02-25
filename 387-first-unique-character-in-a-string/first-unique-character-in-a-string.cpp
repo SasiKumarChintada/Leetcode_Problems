2908348bbf3a742733a5c45eea5fe0d3dffe8091
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-02-25 22:32:31 +0530

    Time: 10 ms (52.86%) | Memory: 14.7 MB (54.34%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n=s.size();
+        unordered_map<char,int>freq;
+        for(char x:s){
+            freq[x]++;
+        }
+        for(int i=0;i<n;i++){
+            if(freq[s[i]]==1) return i;
+        }
+        return -1;
+    }
+};
