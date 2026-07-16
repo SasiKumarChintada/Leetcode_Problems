2e0ccd174a446dc41d8d60112b21ccd28e69baf6
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-07-16 22:43:46 +0530

    Time: 188 ms (5.27%) | Memory: 12.7 MB (21.88%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int>res(boxes.size());
-        int k=0;
-        for(int i=0;i<boxes.size();i++){
-            int sum=0;
-            for(int j=0;j<boxes.size();j++){
-                if(boxes[j]=='1') sum+=abs(i-j);
-            }
-            res[k++]=sum;
+        int n = boxes.size();
+        vector<int> res(n);
+        vector<int> pre(n);
+        pre[0] = 0;
+        int cnt1 = 0;
+        if (boxes[0] == '1')
+            cnt1 = 1;
+        for (int i = 1; i < n; i++) {
+            if (cnt1 >= 1)
+                pre[i] = pre[i - 1] + (cnt1);
+            if (boxes[i] == '1')
+                cnt1++;
+        }
+        for (int i = 0; i < n; i++) {
+            cout << pre[i] << " ";
+        }
+        vector<int> suff(n);
+        suff[n - 1] = 0;
+        int cnt2 = 0;
+        if (boxes[n - 1] == '1')
+            cnt2 = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (cnt2 >= 1)
+                suff[i] = suff[i + 1] + (cnt2);
+            if (boxes[i] == '1')
+                cnt2++;
+        }
+        cout << endl;
+        for (int i = 0; i < n; i++) {
+            cout << suff[i] << " ";
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = pre[i] + suff[i];
         }
         return res;
     }
