fcc91524c5fdd8283ff1a2b459e610a603bb9a05
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-07-23 21:38:28 +0530

    Time: 28 ms (98.43%) | Memory: 46.9 MB (33.28%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>pre(n);
+        pre[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+nums[i];
+        }
+        unordered_map<int,int>freq;
+        freq[0]=1;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int val=pre[i]-k;
+            if(freq.count(val)){
+                cnt+=freq[val];
+            }
+            freq[pre[i]]++;
+        }
+        return cnt;
+    }
+};
