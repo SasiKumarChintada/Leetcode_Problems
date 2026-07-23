a06df746e54d3cfccb2eca4a639fe6aa3ead4c4a
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-07-23 21:50:47 +0530

    Time: 20 ms (24.70%) | Memory: 39.9 MB (9.95%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0;
-        int cnt1=0;
-        int sum1=0;
-        // atmost k subarrays
-        for(int r=0;r<n;r++){
-            sum1+=nums[r];
-            while(sum1>k){
-                sum1-=nums[l];
-                l++;
-            }
-            cnt1+=r-l+1;
+        vector<int>pre(n);
+        pre[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+nums[i];
         }
-        // atmost k-1 subarrays
-        int i=0;
-        int cnt2=0;
-        int sum2=0;
-        for(int j=0;j<n;j++){ 
-            sum2+=nums[j];
-            if(k-1<0) break;
-            while(sum2>k-1){
-                sum2-=nums[i];
-                i++;
+        unordered_map<int,int>freq;
+        freq[0]=1;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int val=pre[i]-k;
+            if(freq.count(val)){
+                cnt+=freq[val];
             }
-                cnt2+=j-i+1;
+            freq[pre[i]]++;
         }
-        return cnt1-cnt2;
+        return cnt;
     }
 };
