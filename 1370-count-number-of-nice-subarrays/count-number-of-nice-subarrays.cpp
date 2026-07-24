78fcbddacded8d89756c924008714ba6529862e2
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-07-24 15:23:21 +0530

    Time: 87 ms (8.41%) | Memory: 97.7 MB (7.11%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int atmost(vector<int>&nums,int k){
-        int n=nums.size();
-        int l=0;
-        int sum=0;
-        int cnt=0;
-        for(int r=0;r<n;r++){
-            sum+=nums[r];
-            while(sum>k){
-                sum-=nums[l];
-                l++;
-            }
-            cnt+=r-l+1;
-        }
-        return cnt;
-    }
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int>res;
         for(int i=0;i<n;i++){
-            if(nums[i]%2!=0) res.push_back(1);
-            else res.push_back(0);
+            if(nums[i]%2==0) nums[i]=0;
+            else nums[i]=1;
+        } 
+        vector<int>pre(n);
+        if(nums[0]%2==0) pre[0]=0;
+        else pre[0]=1;
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+nums[i];
         }
-        return atmost(res,k)-atmost(res,k-1);
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int cnt=0;
+        for(int j=0;j<n;j++){
+            int val=pre[j]-k;
+            if(mp.count(val)){
+                cnt+=mp[val];
+            }
+            mp[pre[j]]++;
+        }
+        return cnt;
     }
 };
