3c0149ee578037820ccdf10d9eb3acd09cff8f13
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-08-06 16:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (20.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,37 +3,34 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
         int low=0,high=n-1;
-        int first=INT_MAX,last=INT_MIN;
-        // finding the first element
+        int minInd=INT_MAX,maxInd=INT_MIN;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]>target){
+            if(nums[mid]==target){
+                minInd=min(minInd,mid);
                 high=mid-1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                first=min(first,mid);
+            else if(nums[mid]>target){
                 high=mid-1;
             }
+            else low=mid+1;
         }
         low=0,high=n-1;
-        //finding the last element
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]>target){
+            if(nums[mid]==target){
+                maxInd=max(maxInd,mid);
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
                 high=mid-1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                last=max(last,mid);
-                low=mid+1;
-            }
+            else low=mid+1;
         }
-        if(first==INT_MAX && last==INT_MIN) return {-1,-1};
-        else return {first,last};
+        if(minInd==INT_MAX && maxInd==INT_MIN) {
+            minInd=-1;
+            maxInd=-1;
+        }
+        return {minInd,maxInd};
     }
 };
