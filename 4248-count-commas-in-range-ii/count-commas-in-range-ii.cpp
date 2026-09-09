abd317821c643ed7c192578f26aacdbae4d68663
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2026-09-09 06:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+        if(n<=999) return 0;
+        else if(n<=999999 && n>=1000){
+            ans+=(n-999);
+        }
+        else if(n<=999999999 && n>=1000000){
+            ans+=(n-999);
+            ans+=(n-999999);
+        }
+        else if(n<=999999999999 && n>=1000000000){
+            ans+=(n-999);
+            ans+=(n-999999);
+            ans+=(n-999999999);
+        }
+        else if(n<=999999999999999 && n>=1000000000000){
+            ans+=(n-999);
+            ans+=(n-999999);
+            ans+=(n-999999999);
+            ans+=(n-999999999999);
+        }
+        else{
+            ans+=(n-999);
+            ans+=(n-999999);
+            ans+=(n-999999999);
+            ans+=(n-999999999999);
+            ans++;
+        }
+        return ans;
+    }
+};
